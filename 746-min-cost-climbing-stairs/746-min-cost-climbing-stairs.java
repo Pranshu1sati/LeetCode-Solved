c47eadd9c157c99a2988c1d50eb37d7f6a55946a
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2022-11-13 19:57:44 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (96.98%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,15 +1,33 @@
 class Solution {
+    
+    
+    
+
+//Greedy    
+//     public int minCostClimbingStairs(int[] cost) {
+        
+//         // Finding Min Of Last Two Element And Adding It With Current Element
+//         for(int i=2; i<cost.length; i++){
+//             cost[i] += Math.min(cost[i-1], cost[i-2]);
+//         }
+        
+//         // Returning The Minimum Of Last 2 Modified Element
+//         return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
+        
+//     }
+// }
+
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        int[] result = new int[n];
+        int dp[] = new int[n+1];
+         
+        dp[n] = 0;
+        dp[n-1]=cost[n-1];
 
-        result[0] = cost[0];
-        result[1] = cost[1];
-
-        for (int i = 2; i < n; i++) {
-            result[i] = cost[i] + Math.min(result[i - 1], result[i - 2]);
+        for (int i = cost.length - 2; i >=0; i--) {
+            dp[i] = cost[i] + Math.min(dp[i + 1], dp[i + 2]);
         }
 
-        return Math.min(result[n - 1], result[n - 2]);
+        return Math.min(dp[0],dp[1]);
     }
 }
