f7479c4c0ce405478cee04147e3c9bad39cb60dd
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2022-11-13 11:30:52 +0530

    Time: 1 ms (88.04%), Space: 42.7 MB (61.41%) - LeetHub

---
+++ 50-powx-n/50-powx-n.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double myPow(double x, int n) {
+
+        if(n==0) return 1;
+        if(x==1 || n==1)
+            {return x;}
+        if(n<0){
+             return (1/x) * myPow(1/x , -(n+1));
+        }
+        double halfpow = myPow(x*x,n/2) ;
+        if(n%2!=0) {return x*halfpow;}
+        else
+        return halfpow;
+    }
+        
+    }
