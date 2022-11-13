c39373bc49e7cb3caf0584a3ecdd9119af2707d9
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2022-11-13 17:11:20 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (53.86%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,7 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
         int a=1; int b =1;
-        for(int i =0 ;  i<n-1; i++){
+        for(int i =n-1 ;  i>=1; i--){
             int temp = a;
             a = a+b;
             b = temp;
