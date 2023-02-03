a19d0fe4a6746f7df111f671e9d3478678fb87c6
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-02-03 12:17:31 +0530

    Time: 0 ms (100.00%), Space: 40 MB (64.44%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.java
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.java
@@ -15,10 +15,10 @@
  */
 class Solution {
     
-    public boolean checkTree(TreeNode root) {
-        if(root==null||(root.left==null && root.right==null)) return true;
-        int left = root.left != null ? root.left.val : 0;
-        int right = root.right != null ? root.right.val : 0;
-        return (left+right==root.val) && checkTree(root.left) && checkTree(root.right);
+    
+    public boolean checkTree(TreeNode root)
+	{
+        return root.val == root.left.val + root.right.val;  // O(1)
     }
+
 }
