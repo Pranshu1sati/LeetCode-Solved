d1e1157d40e2615ad1d67603470a4a5ef2d9f3a3
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-02-13 20:20:53 +0530

    Time: 8 ms (76.88%), Space: 47.4 MB (54.27%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -2,23 +2,26 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         if(intervals.length<=1)
             return intervals;
-        Arrays.sort(intervals,(o1,o2)-> Integer.compare(o1[0],o2[0]));
-        List<int[]> output_arr= new ArrayList();
-        int[]current_interval = intervals[0];
-        output_arr.add(current_interval);
-        for(int[] invterval : intervals){
-            int current_begin=current_interval[0];
-            int current_end=current_interval[1];
-            int next_begin = invterval[0];
-            int next_end = invterval[1];
-            if(current_end >= next_begin){
-                current_interval[1]=Math.max(current_end, next_end);
+  Arrays.sort(intervals,(o1,o2)-> Integer.compare(o1[0],o2[0]));
+List<int[]> result = new ArrayList<>();
+        int []current = intervals[0];
+        result.add(current);
+        for(int i = 1 ; i< intervals.length; i++)
+        {
+            int[] next = intervals[i];
+            if(current[1]>=next[0]){
+                current[1] = Math.max(current[1], next[1]);
             }
-            else{
-                current_interval = invterval;
-                output_arr.add(current_interval);
+            else {
+            current = next;
+            result.add(current);
             }
+            
         }
-        return output_arr.toArray(new int[output_arr.size()][]);
+    int[][] resultArray = new int[result.size()][2];
+    for (int i = 0; i < result.size(); i++) {
+        resultArray[i] = result.get(i);
+    }
+      return resultArray;  
     }
 }
