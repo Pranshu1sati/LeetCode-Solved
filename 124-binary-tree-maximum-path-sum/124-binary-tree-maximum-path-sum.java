60c3a4f1172dc66be3a044d14bd50b02ca84849e
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-02-22 19:21:25 +0530

    Time: 1 ms (97.41%), Space: 48.9 MB (5.48%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -14,28 +14,18 @@
  * }
  */
 class Solution {
-        int curr_sum = 0;
-    int max_sum = 0;
-    int neg_sum = Integer.MIN_VALUE;
-    public int solve(TreeNode root){
-        if(root==null) return 0;
+    int maxValue = Integer.MIN_VALUE;
+    private int maxPathDown(TreeNode node) {
+        if (node == null) return 0;
+        int left = Math.max(0, maxPathDown(node.left));
+        int right = Math.max(0, maxPathDown(node.right));
 
-        
-        int left_sum = solve(root.left);
-        int right_sum = solve(root.right);
-
-        curr_sum = left_sum + right_sum + root.val;
-        if(curr_sum<0) curr_sum = 0;
-        max_sum = Math.max(max_sum,curr_sum);
-        
-        // just in case if we don't find any positive number
-        neg_sum = Math.max(neg_sum,root.val);
-
-        return Math.max(0,(Math.max(left_sum + root.val, right_sum + root.val) ) );
+        maxValue = Math.max(maxValue, left + right + node.val);
+        return Math.max(left, right) + node.val;
     }
     public int maxPathSum(TreeNode root) {
-        solve(root);
-        if(max_sum==0) max_sum = neg_sum;
-        return max_sum;
+        
+     maxPathDown(root);
+        return maxValue;
     }
 }
