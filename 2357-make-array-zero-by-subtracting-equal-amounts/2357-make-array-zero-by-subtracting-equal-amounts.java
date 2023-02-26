e66e61be2526859f228fe0a83a589d61fe9dd0b7
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-02-26 21:34:34 +0530

    Time: 1 ms (93.41%), Space: 40.6 MB (21.98%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,16 +1,11 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        boolean[] arr=new boolean[101];
-         int count=0;
-
-         for(Integer num:nums){
-             arr[num]=true;
-         }
-
-         for(int i=0;i<arr.length;i++){
-             if(arr[i]) count++;
-         }
-
-         return (arr[0])?count-1:count;
+         List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0 && !list.contains(nums[i])) {
+                list.add(nums[i]);
+            }
+        }
+        return list.size();
     }
 }
