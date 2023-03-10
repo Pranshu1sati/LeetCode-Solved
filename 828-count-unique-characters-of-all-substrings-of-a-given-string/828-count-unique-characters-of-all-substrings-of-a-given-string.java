51643930ebf3a0fd05e8cc4b0eb8650eb5a34f41
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-03-10 13:32:04 +0530

    Time: 26 ms (43.10%), Space: 54 MB (13.36%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.java
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.java
@@ -1,29 +1,22 @@
 class Solution {
         public int uniqueLetterString(String s) {
         int n = s.length();
-        List<List<Integer>> hashmap = new ArrayList<>();
-        for (int i = 0; i < 26; i++) {
-            hashmap.add(new ArrayList<>());
-            hashmap.get(i).add(-1);
-        }
-        
-        //Store all indices
+        int[] lastIndex = new int[26];
+        Arrays.fill(lastIndex, -1);
+        int[] f = new int[n];
         for (int i = 0; i < n; i++) {
-            hashmap.get(s.charAt(i) - 'A').add(i);
+            int c = (s.charAt(i) - 'A') ;
+            f[i] = i - lastIndex[c];
+            lastIndex[c] = i;
         }
-        
-        //Push length of string for all keys at the end of each value list
-        for (int i = 0; i < 26; i++) {
-            hashmap.get(i).add(n);
+        Arrays.fill(lastIndex, n);
+        int res = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int c = (s.charAt(i) - 'A') ;
+            f[i] = (lastIndex[c] - i) * f[i];
+            res += f[i];
+            lastIndex[c] = i;
         }
-        
-        long count = 0;
-        for (int i = 0; i < 26; i++) {
-            for (int j = 1; j < hashmap.get(i).size() - 1; j++) {
-                count += (hashmap.get(i).get(j) - hashmap.get(i).get(j - 1)) 
-                    * (hashmap.get(i).get(j + 1) - hashmap.get(i).get(j));
-            }
-        }
-        return (int) (count % 1000000007);
+        return res;
     }
 }
