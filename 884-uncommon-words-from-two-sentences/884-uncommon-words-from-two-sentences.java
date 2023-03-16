bc381deb6bf9da45e21d6f69812fee324358a837
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-03-16 08:56:28 +0530

    Time: 7 ms (8.59%), Space: 42.5 MB (24.91%) - LeetHub

---
+++ 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.java
@@ -0,0 +1,11 @@
+class Solution {
+      public String[] uncommonFromSentences(String A, String B) {
+        Set<String> distinct = new HashSet<>(), com = new HashSet<>();
+        for (String s : (A + " " + B).split("\\s")) {
+            if (com.contains(s) || !distinct.add(s)) { 
+                distinct.remove(s); com.add(s); 
+            }
+        }
+        return distinct.toArray(new String[0]);
+    }
+}
