a5ff0f30e3559dca846535d08b44b6936d2ab8ef
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-03-21 19:45:33 +0530

    Time: 8 ms (66.42%), Space: 42.9 MB (40.48%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -14,18 +14,18 @@
  * }
  */
 class Solution {
-    public void helper(TreeNode root , String s ,List<String> ls ){
+    public void binaryTreePaths4real(TreeNode root , String s ,List<String> ls ){
         if (root.left == null && root.right == null){
              ls.add(s + root.val);
              return;
              }
-    if (root.left != null) helper(root.left, s + root.val + "->", ls);
-    if (root.right != null)helper(root.right, s + root.val + "->", ls);
+    if (root.left != null) binaryTreePaths4real(root.left, s + root.val + "->", ls);
+    if (root.right != null) binaryTreePaths4real(root.right, s + root.val + "->", ls);
     }
     public List<String> binaryTreePaths(TreeNode root) {
        
        List<String> ls = new ArrayList<String>();
-      if(root!=null) helper(root, "", ls);
+      if(root!=null) binaryTreePaths4real(root, "", ls);
       return ls;
          
     }
