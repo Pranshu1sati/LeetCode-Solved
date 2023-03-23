20a36a08dd0ec36048d4db48696c5a47945a1013
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-03-23 16:58:59 +0530

    Time: 2 ms (23.81%), Space: 60.2 MB (14.29%) - LeetHub

--- 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.java
+++ 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.java
@@ -10,18 +10,16 @@ class Solution {
          int max = 0;//for ans;
 
         Stack<Integer> stack = new Stack<>();
-        for (int i = nums.length-1; i >=0; i--) {//pushing elements in reverse order; so that at the time of popoing we get the elements in real array oredering
+        for (int i = nums.length-1; i >=0; i--) {
             stack.push(nums[i]);
         }
-        int index = 0;//we will do the poping for only k-1 times ;beacuse if the stack .peek() is less then previes max then push previous max on the stack at the last move;
+        int index = 0;
         while (index<k-1){
             max = Math.max(stack.pop(), max);
 
             index++;
         }
-        if (k+1<= nums.length) max = Math.max(nums[k],max); // this is for the case where the element below the stack.peek()>stack.peek();
-
-
+        if (k+1<= nums.length) max = Math.max(nums[k],max);
         return max;
     }
 }
