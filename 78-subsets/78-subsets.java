f4bb9f20860f618b7be5cadb38d9da22ddaa4f58
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-05-05 20:56:33 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (13.83%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,17 +1,22 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<Integer> curr = new ArrayList<Integer>();
-        List<List<Integer>>ans  = new ArrayList<>();
-        rec(nums,0,curr,ans);
+       List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        //              123     
+        helper(0 , ans, nums,curr);
         return ans;
     }
-    public static void rec(int [] arr, int i, List<Integer> curr, List<List<Integer>> ans){
-        if (i ==arr.length) {
-            ans.add(new ArrayList<Integer>(curr));
-            return;}
-        curr.add(arr[i]);
-        rec(arr, i + 1, curr, ans);
-        curr.remove(curr.size()-1);
-        rec(arr, i + 1, curr, ans);
+    public void helper(int itr, List<List<Integer>> ans, int[] nums, List<Integer> curr){
+        if(itr == nums.length ){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        
+        curr.add(nums[itr]);
+        
+         helper(itr+1,ans,nums,curr);
+            // remove the element and do the backtracking call.
+         curr.remove(curr.size() - 1);
+         helper(itr+1,ans,nums,curr);
     }
 }
