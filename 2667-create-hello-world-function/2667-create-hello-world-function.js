b857c34294117f10778c435b4decfaf604e81b80
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-05-06 22:13:06 +0530

    Time: 55 ms (100.00%), Space: 42.2 MB (100.00%) - LeetHub

--- 2667-create-hello-world-function/2667-create-hello-world-function.js
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -1,9 +1,13 @@
 /**
  * @return {Function}
  */
-var createHelloWorld = function() {
-    return (...args) => "Hello World";
-};
+// var createHelloWorld = function() {
+//         return () => "Hello World";
+// };
+
+const createHelloWorld =()=>{
+    return ()=>"Hello World"
+}
 /**
  * const f = createHelloWorld();
  * f(); // "Hello World"
