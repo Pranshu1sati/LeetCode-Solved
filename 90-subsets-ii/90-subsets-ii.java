aa19a9a40236dac6e4fc229ce4491f69c9f99c03
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-05-11 13:19:22 +0530

    Time: 1 ms (99.98%), Space: 42.9 MB (49.89%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,26 +1,29 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
+    // List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        
-        subset(0,  nums, nums.length, new ArrayList<>());
-
-        return ans;  
+    List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        //              123     
+        helper(0 , ans, nums,curr);
+        return ans;
     }
-    public void subset(int i, int nums[], int n, List<Integer> curr){
-        if(i == n ){
-            if(ans.contains(curr)){
-                return;
-            }
-            else{
-                ans.add(new ArrayList<>(curr));
-            }
+    public void helper(int i, List<List<Integer>> ans, int[] nums, List<Integer> curr){
+        if(i== nums.length ){
+            ans.add(new ArrayList<>(curr));
             return;
         }
-        subset(i+1, nums, n,curr);
-        curr.add(nums[i]);
-        subset(i+1, nums, n, curr );
-        curr.remove(curr.size()-1);
         
+        curr.add(nums[i]);
+        
+         helper(i+1,ans,nums,curr);
+            // remove the element and do the backtracking call.
+         curr.remove(curr.size() - 1);
+        
+        while(i < nums.length - 1 && (nums[i] == nums[i + 1] ) )
+        {
+            i++ ;
+        }
+         helper(i+1,ans,nums,curr);
     }
 }
