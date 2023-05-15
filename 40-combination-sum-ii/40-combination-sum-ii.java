c365705bcf274fa823da43fe2148aaf3b220847b
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-05-15 20:38:30 +0530

    Time: 3 ms (65.89%), Space: 42.9 MB (52.72%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    List < List < Integer >> ans = new ArrayList <> ();
+    public List<List<Integer>> combinationSum2(int[] c, int t) {
+        Arrays.sort(c);
+        List<Integer> al = new ArrayList<Integer>();
+        sum4real(c, al, t, c.length, 0 );
+        return ans;
+    }
+    private void sum4real(int[] c, List<Integer> al, int  t, int n , int i ){
+        if(i==n){
+            if(t==0) ans.add(new ArrayList<>(al));
+            return;
+        }
+
+        if(t<0) return ;
+        //pick
+        if(c[i]<=t)
+        {al.add(c[i]);
+         sum4real(c, al, t-c[i] , n, i+1 );
+        al.remove(al.size()-1);}
+        while(i + 1 < c.length && c[i] == c[i + 1]) i++ ;
+         sum4real(c,al ,t,n,i+1) ;
+    }
+}
