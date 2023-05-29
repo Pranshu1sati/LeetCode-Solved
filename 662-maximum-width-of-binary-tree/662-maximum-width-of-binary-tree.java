734280f466305ea2f4e8e6b742df5766669831a1
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-05-29 21:38:45 +0530

    Time: 3 ms (12.49%), Space: 43.2 MB (6.59%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -14,36 +14,32 @@
  * }
  */
 class Solution {
-    class Pair {
-    TreeNode node; 
-    int num; 
-    Pair(TreeNode _node, int _num) {
-        num = _num;
-        node = _node; 
-    }
-}
     public int widthOfBinaryTree(TreeNode root) {
-        if(root == null) return 0;
-        int ans = 0;
-        Queue<Pair> q = new LinkedList<>(); 
-        q.offer(new Pair(root, 0)); 
+        Queue<TreeNode> q = new LinkedList<>();
+        HashMap<TreeNode, Integer > mp= new HashMap<>();
+        q.add(root);
+        mp.put(root,0);
+        int currWidth = 0;
+        int MaxWidth = 0;
         while(!q.isEmpty()){
             int size = q.size();
-            int mmin = q.peek().num;    //to make the id starting from zero
-            int first = 0,last = 0;
-            for(int i=0; i<size; i++){
-                int cur_id = q.peek().num-mmin;
-                TreeNode node = q.peek().node;
-                q.poll();
-                if(i==0) first = cur_id;
-                if(i==size-1) last = cur_id;
-                if(node.left != null)
-                    q.offer(new Pair(node.left, cur_id*2+1));
-                if(node.right != null) 
-                    q.offer(new Pair(node.right, cur_id*2+2));
+            int start = 0,end = 0; 
+                for(int i = 0; i < size; i++){
+                TreeNode node = q.poll();
+                if(i == 0) start = mp.get(node);
+                if(i == size - 1) end = mp.get(node);
+                if(node.left != null){
+                    mp.put(node.left, mp.get(node) * 2+1);
+                    q.offer(node.left);
+                }
+                if(node.right != null){
+                    mp.put(node.right, mp.get(node) * 2 + 2);
+                    q.offer(node.right);
+                }
             }
-            ans = Math.max(ans, last-first+1);
+            currWidth =end - start+1;
+            MaxWidth = Math.max(currWidth, MaxWidth);
         }
-        return ans;
+        return MaxWidth;
     }
 }
