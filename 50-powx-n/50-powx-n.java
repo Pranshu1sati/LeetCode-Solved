70d097440581e04416c76f57c327455b8b510bfb
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-07-24 22:28:51 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (98.35%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,7 +1,6 @@
 class Solution {
     public double myPow(double x, int n) {
-
-        if(n==0) return 1;
+       if(n==0) return 1;
         if(x==1 || n==1)
             {return x;}
         if(n<0){
@@ -10,7 +9,6 @@ class Solution {
         double halfpow = myPow(x*x,n/2) ;
         if(n%2!=0) {return x*halfpow;}
         else
-        return halfpow;
+        return halfpow; 
     }
-        
-    }
+}
