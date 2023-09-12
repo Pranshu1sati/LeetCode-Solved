8f7d69b4cf012ec2971bef38c6154c15834c6f69
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-09-13 01:39:13 +0530

    Time: 27 ms (6.09%), Space: 44.4 MB (30.00%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,21 +1,33 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int i = 0;
-        while (i < flowerbed.length) {
-            if (flowerbed[i] == 0) {
-                // Check if the previous and next plots are also empty
-                boolean prevEmpty = (i == 0 || flowerbed[i - 1] == 0);
-                boolean nextEmpty = (i == flowerbed.length - 1 || flowerbed[i + 1] == 0);
-
-                if (prevEmpty && nextEmpty) {
-                    // Plant a flower and decrement n
-                    flowerbed[i] = 1;
-                    n--;
-                }
-            }
+         int size = flowerbed.length;
+        if(n==0) return true;
+        if(size==1 ){
+            if(flowerbed[0]==0 && n ==1) return true;
+            else return false;
+        } 
+        int i =0;
+        while(i<size && n>0) {
+            // System.out.println(i + "before check");
+            if(i==0)
+            {if(flowerbed[i]==0 && flowerbed[i+1] == 0 ) {
+                n--;
+                flowerbed[i] = 1;
+            }}
+            if(i==flowerbed.length-1)
+            {if(flowerbed[i]==0 && flowerbed[i-1] == 0 ) {
+                n--;
+                flowerbed[i] = 1;
+            }}
+            if(i>0 && flowerbed[i]==0 && flowerbed[i-1] !=1 && flowerbed[i+1] !=1  ){
+                System.out.println(i +  "after check");
+                n--;
+                
+                flowerbed[i] =1;
+            } 
             i++;
         }
-
-        return n <= 0;
+        // System.out.println(n);
+        return n==0;
     }
 }
