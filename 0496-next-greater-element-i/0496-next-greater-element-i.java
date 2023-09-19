3828a2b769b80a017544bf3e4c015eebc6366626
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-09-19 11:03:28 +0530

    Time: 4 ms (63.25%), Space: 43.3 MB (69.53%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,44 @@
+class Solution {
+
+// public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+//         HashMap<Integer, Integer> mp = new HashMap<>();
+//         int p = -1;
+//         // int[] reversedNums2 = Arrays.copyOf(nums2, nums2.length);
+//         // reverseArray(reversedNums2);
+//         mp.put(nums2[nums2.length -1],-1);
+//         int max = nums2[nums2.length-1];
+//         for(int i = nums2.length-2; i>=0; i--){
+//           int current= nums2[i];
+//             if(current < max){
+//                 if(i!=nums2.length && nums2[i+1] > current) max = nums2[i+1];
+//                 mp.put(current,max );
+                
+//             }
+//             else mp.put(current,-1);
+            
+//             max = Math.max(current,max);
+            
+//         }
+//         int[] res = new int [nums1.length];
+//         for(int i =0; i< nums1.length; i++){
+//             res[i] = mp.get(nums1[i]);
+//         }
+//         return res;
+//     }
+
+    
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+      HashMap<Integer, Integer> mp = new HashMap<Integer,Integer>();
+    Stack<Integer> st = new Stack<>();
+        for(int num : nums2){
+            while(!st.isEmpty() && st.peek() < num){
+                mp.put(st.pop(),num);
+            }
+        st.push(num);
+        }
+        for( int i =0; i< nums1.length; i++){
+            nums1[i] = mp.getOrDefault(nums1[i],-1);
+        }
+        return nums1;
+    } 
+}
