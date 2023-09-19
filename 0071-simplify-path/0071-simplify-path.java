1e9cbfc9c7a8e712900c9778c3363dea2f77ab68
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-09-19 15:12:08 +0530

    Time: 4 ms (89.30%), Space: 42.2 MB (89.77%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,26 +1,16 @@
 class Solution {
     public String simplifyPath(String path) {
-    String[] parts = path.split("/");
-    Stack<String> stack = new Stack<>();
-
-    for (String part : parts) {
-        if (!part.isEmpty()) {
-            if (part.equals("..")) {
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                }
-            } else if (!part.equals(".")) {
-                stack.push(part);
-            }
+        Stack<String> st = new Stack<>();
+        StringBuilder s = new StringBuilder();
+        String[] p = path.split("/");
+        for(int i =0;i<p.length; i++){
+            if(!st.isEmpty() && p[i].equals("..")) st.pop();
+            else if (!p[i].equals("") && !p[i].equals(".") && !p[i].equals("..")) st.push(p[i]);
         }
+        if(st.isEmpty())return "/";
+        while(!st.isEmpty()){
+            s.insert(0,st.pop()).insert(0,"/");
+        }
+        return s.toString();
     }
-
-    StringBuilder result = new StringBuilder();
-    for (String dir : stack) {
-        result.append("/").append(dir);
-    }
-
-    return result.length() > 0 ? result.toString() : "/";
-}
-    
 }
